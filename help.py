7807abc37e65d089a1c59a96a5b8cd71770472a3
Andrew Murrow <andrew.murrow@kiv.dev>
2022-12-20 21:55:56 +0200

    Django: requirements.txt and help.py update

--- help.py
+++ help.py
@@ -1,3 +1,6 @@
 # python3 -m venv venv -> installing virtual environment
 # source venv/bin/activate -> activating virtual environment
 # deactivate -> deactivate virtual environment
+# python3 manage.py makemigrations
+# python3 manage.py runserver
+# -m pip freeze > requirements.txt
