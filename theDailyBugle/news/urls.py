d1430c8fc386a30fb225ade776a9d3ab766107cb
Andrew Murrow <andrew.murrow@kiv.dev>
2022-12-24 00:34:25 +0200

    Django: routes/category

--- theDailyBugle/news/urls.py
+++ theDailyBugle/news/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('category/<int:category_id>/', get_category)
 ]
--- theDailyBugle/news/views.py
+++ theDailyBugle/news/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
-    return render(request, 'news/index.html', {'news': news, 'title': 'News'})
+    categories = Category.objects.all()
+    context = {
+        'news': news,
+        'title': 'News',
+        'categories': categories
+    }
+
+    return render(request, 'news/index.html', context=context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news': news,
+        'category': category,
+        'categories': categories
+    }
+
+    return render(request, 'news/category.html', context=context)
