719d2b3b578fc0e4dcbefd019a39444aacb9690c
Andrew Murrow <andrew.murrow@kiv.dev>
2022-12-27 00:24:35 +0200

    Django: inclusion tag

--- theDailyBugle/news/templatetags/news_tags.py
+++ theDailyBugle/news/templatetags/news_tags.py
@@ -8,3 +8,9 @@ register = template.Library()
 @register.simple_tag(name='get_list_categories')
 def get_categories():
     return Category.objects.all()
+
+
+@register.inclusion_tag('news/list_categories.html')
+def show_categories():
+    categories = Category.objects.all()
+    return {'categories': categories}
